96f1d8cf9cf39667f913ef0e2c72ec61a4cfaea8
sweetpea22 <cindychau24@me.com>
2023-09-08 16:00:47 -0400

    Add animation

--- src/components/FlyoutMenuItem/index.tsx
+++ src/components/FlyoutMenuItem/index.tsx
@@ -91,7 +91,7 @@ export default function FlyoutMenuItem({
                       key={item.name}
                       className="group relative flex gap-x-6 rounded-2xl p-4 hover:bg-gray-800"
                     >
-                      <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-900 group-hover:bg-gray-800">
+                      <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-900 group-hover:bg-gray-700">
                         <item.icon
                           className="h-6 w-6 text-gray-400 group-hover:text-brand-400"
                           aria-hidden="true"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import Hero from '@/sections/Hero';
-import Head from 'next/head';
 import WhoShouldApply from '@/sections/WhoShouldApply';
 import Why from '@/sections/Why';
 import Offer from '@/sections/Offer';
 import WhatWeLookFor from '@/sections/WhatWeLookFor';
 import CTA from '@/sections/CTA';
-import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
     <>
-    
-      <Navbar />
       <Hero />
       <WhoShouldApply />
       <Why />
       <Offer />
       <WhatWeLookFor />
       <CTA />
-      <Footer />
     </>
   );
 }
