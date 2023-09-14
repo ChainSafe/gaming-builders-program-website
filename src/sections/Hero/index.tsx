4d10b0e9efda38ab61c8300b37e4615ed268d7fd
sweetpea22 <cindychau24@me.com>
2023-09-13 20:04:54 -0400

    change copy

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -10,10 +10,13 @@ export default function Hero() {
     <div className='relative md:min-h-[80vh] h-full bg-bottom bg-cover xl:bg-contain bg-green-bg'>
       <motion.div initial='offscreen'
           whileInView='onscreen' viewport={{amount: 0.4}} className='relative flex flex-col justify-center z-4 px-4 py-40 lg:px-32 lg:py-48'>
-        <motion.h2 variants={fadeInVariant}  className='max-w-4xl z-4 mobile-header-light lg:h1-light'>
-          Build innovative projects using our Gaming SDK and Marketplace
+        <motion.h2 variants={fadeInVariant} className='max-w-4xl z-4 mobile-header-light lg:h1-light'>
+          ChainSafe Gaming Builder Program
         </motion.h2>
-        <motion.p variants={fadeInVariant}  className='h4-light max-w-4xl pb-8'>
+        <motion.p variants={fadeInVariant}  className='h4-light max-w-4xl pb-2'>
+         Build innovative projects using our Gaming SDK and Marketplace
+        </motion.p>
+        <motion.p variants={fadeInVariant} className='text-lg text-gray-300 max-w-2xl pb-8'>
           The ChainSafe Gaming Builders Program offers funding, guidance, and
           support to developers interested in building creative projects with
           ChainSafe Gaming.
